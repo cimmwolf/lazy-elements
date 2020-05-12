a41617bc0436734ebfd9cd3de62923c452eef3a8
cimmwolf <cimmwolf@gmail.com>
2020-05-12 08:55:07 +0400

    Change delayed-* elements to lazy-*

--- lazy-img.js
+++ lazy-img.js
@@ -1,15 +1,15 @@
-let images = document.querySelectorAll('delayed-img');
+let images = document.querySelectorAll('lazy-img');
 for (let i = 0, len = images.length; i < len; i++) {
   let frame = images[i];
   try {
     let observer = new IntersectionObserver((entries) => {
       if (entries[0].isIntersecting) {
-        frame.outerHTML = frame.outerHTML.replace(/delayed-img/g, "img");
+        frame.outerHTML = frame.outerHTML.replace(/lazy-img/g, "img");
         observer.disconnect();
       }
     }, {threshold: [0], rootMargin: '0px 0px ' + document.documentElement.clientHeight + 'px'});
     observer.observe(frame);
   } catch (e) {
-    frame.outerHTML = frame.outerHTML.replace(/delayed-img/g, "img");
+    frame.outerHTML = frame.outerHTML.replace(/lazy-img/g, "img");
   }
 }
